797796d2a318f7c5562dd6a7bfec5b99714118b1
Sohini3018 <sohinismail@gmail.com>
2024-05-21 16:14:45 +0530

    added suppressWarning annotations and ignored bin folders in gitignore

--- eduHub-Backend/src/main/java/com/example/myapp/configs/SecurityConfiguration.java
+++ eduHub-Backend/src/main/java/com/example/myapp/configs/SecurityConfiguration.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -27,6 +26,7 @@ public class SecurityConfiguration {
         this.jwtAuthenticationFilter = jwtAuthenticationFilter;
     }
     
+    @SuppressWarnings("removal")
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
